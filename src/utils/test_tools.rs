b68fbe8ea6b731c9893f1767f9e2a3aa1c279c85
lonelyhentai <master@evernightfireworks.com>
2019-12-29 12:24:50 +0800

    docs(readme,license): fill out readme and license, write readme table generator
    feat(string): task 301 remove invalid parentheses
    docs(readme,license): add readme and license,
    write readme generator

--- src/utils/test_tools.rs
+++ src/utils/test_tools.rs
@@ -3,6 +3,12 @@ use std::iter::FromIterator;
 use std::hash::Hash;
 use std::fmt::Debug;
 
+pub fn assert_equivalent<T: Eq + Ord + Hash + Debug + Clone>(left: &[T], right: &[T]) {
+    let left = left.iter().cloned().collect::<HashSet<_>>();
+    let right = right.iter().cloned().collect::<HashSet<_>>();
+    assert_eq!(left, right);
+}
+
 pub fn assert_nested_equivalent<T: Eq + Ord + Hash + Debug>(left: &[Vec<T>], right: &[Vec<T>]) {
     let left = left.iter()
         .map(|x| BTreeSet::from_iter(x.iter())).collect::<HashSet<_>>();
@@ -21,4 +27,11 @@ pub fn map_nested_to_string(strs: &[Vec<&str>]) -> Vec<Vec<String>> {
 
 pub fn assert_feq(p: f64, q: f64) {
     assert!(f64::abs(p-q)< 1e-64);
+}
+
+#[macro_export]
+macro_rules! to_s {
+  ($elem: expr) => {
+    String::from( $elem )
+  }
 }
